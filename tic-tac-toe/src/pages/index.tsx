3dea9e238d666022c321f0f56859c625afcb5010
manny-espinoza <mannyespinoza123@yahoo.com>
2023-03-22 16:35:07 -0700

    Added website tab icon, and hover styling

--- tic-tac-toe/src/pages/index.tsx
+++ tic-tac-toe/src/pages/index.tsx
@@ -16,10 +16,7 @@ export default function Home() {
           rel="stylesheet"
         ></link>
         <link rel="preconnect" href="https://fonts.googleapis.com"></link>
-        <link
-          rel="preconnect"
-          href="https://fonts.gstatic.com"
-        ></link>
+        <link rel="preconnect" href="https://fonts.gstatic.com"></link>
         <link
           href="https://fonts.googleapis.com/css2?family=Finger+Paint&display=swap"
           rel="stylesheet"
